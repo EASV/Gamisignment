1705bc3f915d73e29dac1a3927d532e4c1665c62
lbilde <larsbilde@gmail.com>
2016-09-21 12:45:26 +0200

    Initial Commit

---
+++ GamisignmentDll/Context/GamiSignmentContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GamisignmentDll.Entities;
+
+namespace GamisignmentDll.Context
+{
+    public class GamiSignmentContext : DbContext
+    {
+        public GamiSignmentContext() : base()
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Chore> Chores { get; set; }
+        public DbSet<Prize> Prizes { get; set; }
+        public DbSet<Repeat> Repeats { get; set; }
+
+
+    }
+}
---
+++ GamisignmentDll/Managers/PrizeManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GamisignmentDll.Context;
+using GamisignmentDll.Entities;
+
+namespace GamisignmentDll.Managers
+{
+    class PrizeManager : IManager<Prize>
+    {
+        public List<Prize> Read()
+        {
+            using (var db = new GamiSignmentContext())
+            {
+                return db.Prizes.ToList();
+            }
+        }
+
+        public Prize Read(int id)
+        {
+            using (var db = new GamiSignmentContext())
+            {
+                return db.Prizes.FirstOrDefault(x => x.Id == id);
+            }
+        }
+
+        public Prize Create(Prize t)
+        {
+            using (var db = new GamiSignmentContext())
+            {
+                db.Prizes.Add(t);
+                db.SaveChanges();
+                return t;
+            }
+        }
+
+        public Prize Update(Prize t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
